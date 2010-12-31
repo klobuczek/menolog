56f16b0017b4980e344abd8289889e95b0cb7006
Heinrich Klobuczek <heinrich@mail.com>
2010-12-30 16:11:31 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Menolog::Application.config.secret_token = '075e94a7b1d5b7ca188c8db6a54c2d0f4b77b35659e0a332bd75b06b9af50fa2d6ae5b9c7c38504d8d0ce6e2252e89e9ebe36bbff58b462137ff75659a6ca2f6'
